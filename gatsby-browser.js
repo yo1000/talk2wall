f6d4cdd32408e947c6ff09a1cd833cc912853e90
YO!CHI KIKUCHI <i@yo1000.com>
2020-08-22 14:00:23 +0900

    Fix style on width overflow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "./src/styles/html.css"
+import "./src/styles/base.css"
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
 import "prismjs/themes/prism-tomorrow.css"
