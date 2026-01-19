429b73ee08c5ee6873f48e456a1e255cb4f40630
YO!CHI KIKUCHI <i@yo1000.com>
2026-01-20 01:01:41 +0900

    Add title each post support

--- src/app/posts/[path]/page.tsx
+++ src/app/posts/[path]/page.tsx
@@ -11,31 +11,40 @@ export const revalidate = false;
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-    return getPosts()
-        .map(p => ({path: p.meta.path?.replace(/^\//, "")}));
+  return getPosts()
+    .map(p => ({path: p.meta.path?.replace(/^\//, "")}));
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ path: string }> }) {
+  const { path } = await params;
+  const post = getPosts().find(p => p.meta.path?.replace(/^\//, "") === path);
+
+  return {
+    title: `${post?.meta?.title} | talk2wall`,
+  };
 }
 
 export default async function PostsPathPage({ params }: { params: Promise<{ path: string }> }) {
-    const { path } = await params;
-    const post = getPosts().find(p => p.meta.path?.replace(/^\//, "") === path);
+  const { path } = await params;
+  const post = getPosts().find(p => p.meta.path?.replace(/^\//, "") === path);
 
-    if (!post) return notFound();
+  if (!post) return notFound();
 
-    const assets = extractAssets(post.content.origin);
-    for (const asset of assets) {
-        copyAssets({
-            absSrcDir: paths.dirname(post.meta.absolutePath),
-            relDestDir: path,
-            imagePath: asset,
-        });
-    }
+  const assets = extractAssets(post.content.origin);
+  for (const asset of assets) {
+    copyAssets({
+      absSrcDir: paths.dirname(post.meta.absolutePath),
+      relDestDir: path,
+      imagePath: asset,
+    });
+  }
 
-    return (
-      <div className={styles.split}>
-        <PostContent title={post?.meta?.title ?? ""}>
-          <div dangerouslySetInnerHTML={{__html: post?.content?.html ?? ""}}></div>
-        </PostContent>
-        <Sidebar meta={post.meta}/>
-      </div>
-    );
+  return (
+    <div className={styles.split}>
+      <PostContent title={post?.meta?.title ?? ""}>
+        <div dangerouslySetInnerHTML={{__html: post?.content?.html ?? ""}}></div>
+      </PostContent>
+      <Sidebar meta={post.meta}/>
+    </div>
+  );
 }
